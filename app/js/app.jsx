87b46336e57798ea4faa1c4be6a1f0799e09a1c2
Andrey Klymets <bipycxp@gmail.com>
2016-10-03 00:45:16 +0300

    Add Material UI
    [v-3.0.0]

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
 
 import App from 'Components/App'
 
-ReactDOM.render((<App />), document.getElementById('root'))
+const WrappedApp = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+)
+
+ReactDOM.render((<WrappedApp />), document.getElementById('root'))
