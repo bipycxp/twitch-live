4afb423bc9433b72afcc4d4d9acd8876b89826cf
Andrey Klymets <bipycxp@gmail.com>
2016-10-12 04:36:09 +0300

    Add toNow util
    [v-3.0.0]
    
    - Add tests

---
+++ app/js/utils/toNow.js
@@ -0,0 +1,31 @@
+import moment from 'moment'
+
+/**
+ * Get formatted time from `started` to now.
+ *
+ * @param {string} started In ISO format.
+ * @param {boolean=} short If true - return time in short format.
+ * @return {string}
+ */
+export default function toNow (started, short = false) {
+  let momentStarted = moment(started)
+
+  // Return time in short format if parameter `short` is true.
+  if (short) return momentStarted.toNow(true)
+
+  // Count diff between `started` and now. Don't forget UTC format.
+  let diff = moment(moment().diff(momentStarted)).utc().toObject()
+
+  let days = diff.date - 1 // Dates starts from 1, but we need count of the days, so remove one.
+  let { hours, minutes } = diff
+
+  let time = []
+
+  // If time field isn't empty - push to `time` with short name.
+  days && time.push(days + 'd')
+  hours && time.push(hours + 'h')
+  minutes && time.push(minutes + 'm')
+
+  // Return string with space between time fields.
+  return time.join(' ')
+}
---
+++ test/utils/toNow.test.js
@@ -0,0 +1,88 @@
+import test from 'ava'
+import sinon from 'sinon'
+
+import toNow from 'utils/toNow'
+
+const NOW = '2016-10-12T00:00:00.000Z'
+
+const tests = [
+  {
+    title: 'Started day ago(full)',
+    entry: {
+      started: '2016-10-11T00:00:00.000Z',
+      short: false
+    },
+    expected: '1d'
+  },
+  {
+    title: 'Started day ago(short)',
+    entry: {
+      started: '2016-10-11T00:00:00.000Z',
+      short: true
+    },
+    expected: 'a day'
+  },
+  {
+    title: 'Started one hour ago(full)',
+    entry: {
+      started: '2016-10-11T23:00:00.000Z',
+      short: false
+    },
+    expected: '1h'
+  },
+  {
+    title: 'Started one hours ago(short)',
+    entry: {
+      started: '2016-10-11T23:00:00.000Z',
+      short: true
+    },
+    expected: 'an hour'
+  },
+  {
+    title: 'Started 10 minutes ago(full)',
+    entry: {
+      started: '2016-10-11T23:50:00.000Z',
+      short: false
+    },
+    expected: '10m'
+  },
+  {
+    title: 'Started 10 minutes ago(short)',
+    entry: {
+      started: '2016-10-11T23:50:00.000Z',
+      short: true
+    },
+    expected: '10 minutes'
+  },
+  {
+    title: 'Started ~2 days ago(full)',
+    entry: {
+      started: '2016-10-10T01:35:25.000Z',
+      short: false
+    },
+    expected: '1d 22h 24m'
+  },
+  {
+    title: 'Started ~2 days ago(short)',
+    entry: {
+      started: '2016-10-10T01:35:25.000Z',
+      short: true
+    },
+    expected: '2 days'
+  },
+  {
+    title: 'Test default `short` parameter',
+    entry: {
+      started: '2016-10-11T00:00:00.000Z'
+    },
+    expected: '1d'
+  }
+]
+
+tests.forEach(async ({ title, entry, expected }) => test(title, async (t) => {
+  // Spy Date.now.
+  Date.now = sinon.spy(() => NOW)
+
+  const time = toNow(entry.started, entry.short)
+  t.is(time, expected)
+}))
